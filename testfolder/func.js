6788a7f34a2d80e55cb36650c98c00af7351eb2a
理絵 <>
2018-12-20 19:31:10 +0900

    ファイル移動

---
+++ testfolder/func.js
@@ -0,0 +1,17 @@
+// jquery.masorny.js ギャラリーを並べる
+jQuery(function($){
+
+  var container = document.querySelector('#gallery');
+
+  imagesLoaded(container, function () {
+    var msnry = new Masonry(container, {
+      itemSelector: '.item', //コンテンツのclass名
+      isFitWidth: true, //コンテナの親要素のサイズに基づいて、コンテンツのカラムを自動調節します。
+      columnWidth: 310, //カラムの幅を設定
+    });
+
+    $('#loading').fadeOut(300); //画像が読み込み終わったらloadingを非表示にする
+    $('#gallery').addClass('on'); //コンテナにclassを付与して表示を切り替える
+
+  });
+});
