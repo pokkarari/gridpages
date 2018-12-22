6fa639fce3d2f4b86d676ba65a9906a5393ae178
理絵 <>
2018-12-23 03:33:27 +0900

    ギャラリー手直し

--- js/func.js
+++ js/func.js
@@ -1,18 +1,12 @@
 // jquery.masorny.js ギャラリーを並べる
-jQuery(function($){
 
-
-  var container = document.querySelector('#gallery');
-
-  imagesLoaded(container, function () {
-    var msnry = new Masonry(container, {
-      itemSelector: '.item', //コンテンツのclass名
-      isFitWidth: true, //コンテナの親要素のサイズに基づいて、コンテンツのカラムを自動調節します。
-      columnWidth: 310, //カラムの幅を設定
+    $(function (){
+  var $container = $("#content");
+  $conteniner.imagesloaded(function(){
+  $conteniner.masonry({
+    itemSelector: ".item";//コンテンツのclass名
+    columnWidth: 250;//カラムの幅を設定
+    isFitWidth: true;//コンテナの親要素のサイズに基づいて、コンテンツのカラムを自動調節します。
+      })
     });
-
-    $('#loading').fadeOut(300); //画像が読み込み終わったらloadingを非表示にする
-    $('#gallery').addClass('on'); //コンテナにclassを付与して表示を切り替える
   });
-
-});
