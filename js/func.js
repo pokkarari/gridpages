9dce586a076627bd84d68fd6d8e80efcef50bc4e
理絵 <>
2018-12-24 15:41:22 +0900

    images の整理

--- js/func.js
+++ js/func.js
@@ -1,16 +1,6 @@
 // jquery.masorny.js ギャラリーを並べる
-    // $(function () {
-    //   var $container = $('#content');
-    //   $container.imagesLoaded(function () {
-    //     $container.masonry({
-    //       itemSelector: '.item',//コンテンツのclass名
-    //       columnWidth: 250,//カラムの幅を設定
-    //       isFitWidth: true /コンテナの親要素のサイズに基づいて、コンテンツのカラムを自動調節します。
-    //     });
-    //   });
-    // });
     $(function (){
-  var $container = $("#content");
+  var $container = $("#gallery_content");
   $container.imagesLoaded(function(){
   $container.masonry({
     itemSelector: ".item",//コンテンツのclass名
