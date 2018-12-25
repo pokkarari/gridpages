b6525535cc65d50ca487e66a383a99e788252262
理絵 <>
2018-12-26 02:55:46 +0900

    PHPをどうにか持ってくる

--- htdocs/admin/newfile.php
+++
@@ -1,8 +0,0 @@
-<?php
-//データの追加
-$sql = "UPDATE news
-                    SET
-                    posted = ?,
-                    title = ?,
-                    message = ?,
-                    WHERE id = ?";
---
+++ htdocs/admin/php/news_edit_done.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>お知らせの編集完了 | Crescent Shoes 管理</title>
+<link rel="stylesheet" href="css/admin.css">
+</head>
+<body>
+<header>
+  <div class="inner">
+    <span><a href="index.php">Crescent Shoes 管理</a></span>
+    <div id="account">
+      admin
+      [ <a href="logout.php">ログアウト</a> ]
+    </div>
+  </div>
+</header>
+<div id="container">
+  <main>
+    <h1>お知らせの編集</h1>
+    <p>お知らせの編集が完了しました。</p>
+    <p><a href="index.php">お知らせ一覧へ戻る</a></p>
+  </main>
+  <footer>
+    <p>&copy; Crescent Shoes All rights reserved.</p>
+  </footer>
+</div>
+</body>
+</html>
